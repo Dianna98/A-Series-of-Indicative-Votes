984a3122deef3b95cda3c5fd3ece21b695e837e4
Diana Crintea <47833224+Dianna98@users.noreply.github.com>
2019-05-16 22:10:25 +0100

    finished commenting

--- src/Coordinator.java
+++ src/Coordinator.java
@@ -21,7 +21,50 @@ public class Coordinator {
     private boolean check = true;
     private String removed;
 
+    public static void main(String[] args) {
 
+        int port = Integer.parseInt(args[0]);
+        int parts = Integer.parseInt(args[1]);
+        Set<String> options = new HashSet<>();
+
+        for (int i = 2; i < args.length; i++) {
+            options.add(args[i]);
+        }
+
+        // create new Coordinator
+        new Coordinator(port, parts,options);
+
+    }
+
+    public Coordinator(Integer port, Integer parts, Set<String> options){
+
+        this.port = port;
+        this.parts = parts;
+        this.options = options;
+
+        // print information for each round
+        System.out.println("_____________________________________________");
+        System.out.println("COORDINATOR " + port + " is starting a new round...");
+        System.out.println("List of options : "+ options.toString());
+        System.out.println("Number of expected participants : " + parts);
+        System.out.println("---------------------------------------------");
+
+        try {
+            // open ServerSocket which accepts sockets from participants
+            serverSocket = new ServerSocket(this.port);
+
+            while (count<2*parts){
+                Socket socket = serverSocket.accept();
+                count++;
+                // start thread that communicates with the participant
+                new CoordinatorThread(socket).start();
+            }
+
+        } catch (IOException e) {
+        }
+    }
+
+    // this class describes the main thread that assures correspondence with participants
     private class CoordinatorThread extends Thread{
 
         private Socket socket;
@@ -46,13 +89,18 @@ public class Coordinator {
             try {
                 while ((message = in.readLine())!=null) {
 
+                    // get JOIN messages
                     if (message.contains("JOIN")) {
                         System.out.println(message);
                         String[] protocol = message.split(" ");
+
+                        // add participant in the participants set
                         participants.put(Integer.valueOf(protocol[1]), socket);
 
+                        System.out.println("PARTICIPANT " + protocol[1] + " has joined.");
                     }
 
+                    // when all participants joined the game, send them DETAILS and VOTE_OPTIONS
                     if ((participants.size() == parts) && send) {
 
                         send = false;
@@ -63,6 +111,7 @@ public class Coordinator {
                             votes = votes + " " + opt;
                         }
 
+
                         for (Socket s : participants.values()) {
                             String details = "DETAILS";
                             out = new PrintWriter(new OutputStreamWriter(s.getOutputStream()), true);
@@ -72,54 +121,68 @@ public class Coordinator {
                                     details = details + " " + p.getKey();
                                 }
                             }
-
                             out.println(details);
                             out.println(votes);
                         }
 
                     }
 
+                    // get OUTCOME messages
                     if (message.contains("OUTCOME")) {
                         outputCount++;
                         String[] outcome = message.split(" ");
 
                         if (!message.contains("none")) {
+
+                            // if the outcome is coming from an active participant then print the message
                             System.out.println(message);
                             Set<Integer> ports = new HashSet<>();
+
+                            // if the outcome is a tie save the option to be removed and add the participants to the list
                             if (outcome[1].contains("null")) {
+
                                 removed = outcome[2];
                                 for (int i = 3; i < outcome.length; i++) {
                                     ports.add(Integer.parseInt(outcome[i]));
                                 }
+
                             } else {
+                                // if the outcome is decided, add the participants to the list
                                 for (int i = 2; i < outcome.length; i++) {
                                     ports.add(Integer.parseInt(outcome[i]));
                                 }
+
                             }
                             outcomes.put(outcome[1], ports);
+
                         } else {
+                            // if the outcome comes from a failed participant, add it to the list of failed participants
                             failed.add(Integer.parseInt(outcome[2]));
                         }
                     }
 
+                    // if the number of outputs received is the same as the number of registered participants then stop looking for messages
                     if (outputCount==parts){
                         stop = true;
                         break;
                     }
                 }
 
+                // if the number of outputs is meet and if is the first time executing the structure
                 if (stop && check) {
-                    //sleep(1000);
 
                     check = false;
 
+                    // print information about the round and the outcome
                     System.out.println(getErrorMessage());
                     System.out.println(getOutcomeMessage());
 
+                    // if the outcome is null (i.e. the participants did not meet a majority) then remove an option from the list and restart the game
                     if (getOutcome().contains("null")){
-                        System.out.println("Majority not met!");
+                        System.out.println("Majority not met! A new round will begin");
                         serverSocket.close();
                         options.remove(removed);
+                        System.out.println("Option " + removed + " has been removed from the options list.");
                         new Coordinator(port,parts-failed.size(),options);
                     }
                 }
@@ -132,6 +195,7 @@ public class Coordinator {
 
     }
 
+    // this method returns the final outcome
     public String getOutcome(){
         if(outcomes.size()==1){
             for (String o : outcomes.keySet()) {
@@ -141,6 +205,7 @@ public class Coordinator {
         return null;
     }
 
+    // this method returns the final message regarding failed participants
     public String getErrorMessage(){
         if (failed.isEmpty()){
             return "All participants have sent their outcome.";
@@ -153,6 +218,7 @@ public class Coordinator {
         }
     }
 
+    // this method returns a message regarding the final outcome
     public String getOutcomeMessage() {
         if (outcomes.isEmpty()) {
             return "All participants failed in sending an outcome.";
@@ -162,45 +228,6 @@ public class Coordinator {
             }
         }
         return "The participants did not agree.";
-    }
-
-
-    public Coordinator(Integer port, Integer parts, Set<String> options){
-
-        this.port = port;
-        this.parts = parts;
-        this.options = options;
-
-        System.out.println("Starting a new round...");
-        System.out.println("COORDINATOR " + port);
-        System.out.println("List of options : "+ options.toString());
-
-        try {
-            serverSocket = new ServerSocket(this.port);
-
-            while (count<2*parts){
-                Socket socket = serverSocket.accept();
-                count++;
-                new CoordinatorThread(socket).start();
-            }
-
-        } catch (IOException e) {
-        }
-    }
-
-    public static void main(String[] args) {
-
-        int port = Integer.parseInt(args[0]);
-        int parts = Integer.parseInt(args[1]);
-        Set<String> options = new HashSet<>();
-
-        for (int i = 2; i < args.length; i++) {
-            options.add(args[i]);
-        }
-
-
-        new Coordinator(port, parts,options);
-
     }
 
 }
--- src/Participant.java
+++ src/Participant.java
@@ -16,6 +16,38 @@ public class Participant {
     private String vote;
     private ServerSocket serverSocket;
 
+    public static void main(String[] args) {
+
+        int cport = Integer.parseInt(args[0]);
+        int pport = Integer.parseInt(args[1]);
+        int timeout = Integer.parseInt(args[2]);
+        int flag = Integer.parseInt(args[3]);
+
+        // create new Participant
+        new Participant(cport,pport,timeout,flag);
+
+    }
+
+    public Participant(Integer cport, Integer pport, Integer timeout, Integer flag){
+
+        this.cport = cport;
+        this.pport = pport;
+        this.timeout = timeout;
+        this.flag = flag;
+
+        try {
+            // open socket to connect to the ServerSocket from the Coordinator
+            Socket socket = new Socket("localhost",cport);
+
+            // start thread that communicates with the Coordinator
+            new ParticipantThread(socket).start();
+
+        } catch (IOException e) {
+        }
+
+    }
+
+    // this class describes the main thread that assures the correspondence with the coordinator
     private class ParticipantThread extends Thread{
 
         private Socket socket;
@@ -40,11 +72,13 @@ public class Participant {
 
                 // send JOIN
                 String join = "JOIN " + pport;
+                System.out.println("PARTICIPANT "+ pport + " registered with the COORDINATOR.");
                 out.println(join);
 
                 // get DETAILS
                 String message = in.readLine();
                 System.out.println(message);
+                System.out.println("PARTICIPANT "+ pport + " got the DETAILS from the COORDINATOR.");
 
                 String[] details = message.split(" ");
                 for (int i=1; i<details.length;i++){
@@ -54,6 +88,7 @@ public class Participant {
                 // get VOTE_OPTIONS
                 message = in.readLine();
                 System.out.println(message);
+                System.out.println("PARTICIPANT "+ pport + " got the VOTE_OPTIONS from the COORDINATOR.");
 
                 String[] v = message.split(" ");
                 for (int i = 1; i < v.length; i++) {
@@ -67,16 +102,12 @@ public class Participant {
                 System.out.println("PARTICIPANT " + pport + " voted "+vote+".");
                 votes.put(pport,vote);
 
+                // close the connection with the Coordinator
                 socket.close();
 
                 // receive votes from other participants
                 Thread search = new StartSearching();
                 search.start();
-//                sleep(timeout);
-                //search.interrupt();
-                //search.
-
-                //sleep(200);
 
                 // send vote to the other participants
                 Thread sending = new StartSending();
@@ -87,60 +118,70 @@ public class Participant {
                 serverSocket.close();
                 sleep(500);
 
+                // if the participant did not fail during sending the vote
                 if (flag != 1) {
-                    //System.out.println("No. votes : "+votes.size());
-                    //System.out.println("No. participants : "+participants.size());
 
-                    // receive all votes from other participants
+                    // receive all lists of votes from other participants
                     Thread searchAll = new SearchAllVotes();
                     searchAll.start();
 
                     sleep(500);
 
+                    // send list of all votes to the other participants
                     Thread sendAllVotes = new SendAllVotes();
                     sendAllVotes.start();
+
                     sleep(timeout);
                     serverSocket.close();
                 }
 
                 sleep(timeout);
 
-                //sleep(1000);
+                // reopen the connection with the Coordinator
                 socket = new Socket("localhost",cport);
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
 
+                // if the participant fails right before sending the output, the participant sends an error signal to the Coordinator
                 if (flag == 2){
                     String outcome = "OUTCOME none " + pport;
                     out.println(outcome);
                     System.out.println("PARTICIPANT " + pport + " failed right before sending the output.");
 
+                // if the participant fails while sending his vote, then it fails sending the outcome as well
                 } else if (flag == 1){
                     String outcome = "OUTCOME none " + pport;
                     out.println(outcome);
 
+                // if the participant does not fail
                 } else if (flag == 0) {
-                    // decide outcome
+                    // then it decides the outcome
                     String outcome = decideOutcome(votes);
                     Set<Integer> whoVoted = votes.keySet();
 
                     String outcomeMessage = "OUTCOME " + outcome;
 
-                    if (outcome==null){
+                    // if the outcome is null (i.e. there is a tie or no majority meet) the outcome is of form
+                    // OUTCOME null <option to be removed> [<participant port>]
+                    if (outcome == null) {
                         outcomeMessage = outcomeMessage + " " + removeOption(votes);
                     }
+
                     for (Integer p : whoVoted) {
                         outcomeMessage = outcomeMessage + " " + p;
                     }
                     System.out.println(outcomeMessage);
 
-                   out.println(outcomeMessage);
+                    out.println(outcomeMessage);
+
+                    System.out.println("PARTICIPANT " + pport + " sent the OUTPUT to the COORDINATOR.");
 
                     System.out.println("PARTICIPANT " + pport + " did not fail.");
 
-                    if (outcome == null){
+                    // if the majority is not met while voting, the participant joins a new round
+                    if (outcome == null) {
                         sleep(1000);
-                        new Participant(cport,pport,timeout,flag);
+                        new Participant(cport, pport, timeout, flag);
                     }
                     //System.exit(0);
                 }
@@ -150,6 +191,7 @@ public class Participant {
 
         }
 
+        // this method returns a random vote from the options list
         public String randomiseVote(Set<String> options){
             int item = new Random().nextInt(options.size());
             int i = 0;
@@ -164,33 +206,7 @@ public class Participant {
 
     }
 
-    public Participant(Integer cport, Integer pport, Integer timeout, Integer flag){
-
-        this.cport = cport;
-        this.pport = pport;
-        this.timeout = timeout;
-        this.flag = flag;
-
-        try {
-            Socket socket = new Socket("localhost",cport);
-            new ParticipantThread(socket).start();
-
-        } catch (IOException e) {
-        }
-
-    }
-
-    public static void main(String[] args) {
-
-        int cport = Integer.parseInt(args[0]);
-        int pport = Integer.parseInt(args[1]);
-        int timeout = Integer.parseInt(args[2]);
-        int flag = Integer.parseInt(args[3]);
-
-        new Participant(cport,pport,timeout,flag);
-
-    }
-
+    // this method returns a set of the options and the number of participants who voted that option
     public HashMap<String,Integer> getVotes (HashMap<Integer,String> votes){
         HashMap<String,Integer> out = new HashMap<>();
         int nr;
@@ -206,6 +222,7 @@ public class Participant {
         return out;
     }
 
+    // this method decides the outcome
     public String decideOutcome(HashMap<Integer,String> votes){
         int parts = votes.size();
         int majority = parts/2+1;
@@ -220,21 +237,7 @@ public class Participant {
         return null;
     }
 
-    public Set<Integer> getMajority(HashMap<Integer,String> votes){
-        String vote = decideOutcome(votes);
-        Set<Integer> out = new HashSet<>();
-
-        if (vote!=null) {
-            for (Map.Entry<Integer, String> v : votes.entrySet()) {
-                if (v.getValue().equals(vote)) {
-                    out.add(v.getKey());
-                }
-            }
-        }
-        return out;
-    }
-
-    // this method returns one of the least voted options
+    // this method returns one of the least voted options in order to remove it in the next round of voting
     public String removeOption(HashMap<Integer,String> votes){
         HashMap<String,Integer> stats = getVotes(votes);
         int min = Integer.MAX_VALUE;
@@ -253,6 +256,7 @@ public class Participant {
         return null;
     }
 
+    // this class describes the thread which opens a new ServerSocket for receiving votes from the other participants
     private class StartSearching extends Thread{
         StartSearching(){}
 
@@ -261,6 +265,7 @@ public class Participant {
             super.run();
 
             try {
+                // create new ServerSocket that accepts from other participants
                 serverSocket = new ServerSocket(pport);
                 while (count<participants.size()){
                     Socket socket = serverSocket.accept();
@@ -273,6 +278,7 @@ public class Participant {
         }
     }
 
+    // this class describes the thread which receives votes from the other participants
     private class GetVotes extends Thread{
 
         Socket socket;
@@ -293,6 +299,9 @@ public class Participant {
                 String message = in.readLine();
                 String[] vote = message.split(" ");
                 System.out.println(message);
+                System.out.println("PARTICIPANT " + pport + " received vote from PARTICIPANT " + vote[1] +".");
+
+                // add the vote received in the HashMap of votes
                 votes.put(Integer.parseInt(vote[1]), vote[2]);
 
             } catch (IOException e) {
@@ -300,6 +309,7 @@ public class Participant {
         }
     }
 
+    // this class describes the thread which sends the vote to the other participants
     private class StartSending extends Thread{
 
         StartSending(){}
@@ -311,17 +321,22 @@ public class Participant {
             int random = participants.size();
             int i=0;
 
+            // if the participant is bound to fail during setp 4 (i.e. while sending his vote)
+            // he fails after sending a random number of votes between 1 and the number of participants -1
             if (flag == 1){
                 random = new Random().nextInt(participants.size()-1);
             }
+
             random++;
             for (Integer p : participants){
                 if (i<random) {
                     try {
+                        // create new socket to connect to other participants
                         Socket socket = new Socket("localhost", p);
 
                         PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
                         out.println("VOTE " + pport + " " + vote);
+                        System.out.println("PARTICIPANT " + pport + " sent his vote to PARTICIPANT " + p + ".");
 
                         sleep(500);
                         socket.close();
@@ -343,6 +358,7 @@ public class Participant {
         }
     }
 
+    // this class describes the thread which opens the ServerSocket for receiving the lists with new votes
     private class SearchAllVotes extends Thread {
 
         SearchAllVotes(){}
@@ -352,11 +368,11 @@ public class Participant {
             super.run();
 
             try {
+                // create new ServerSocket that accepts from other participants
                 serverSocket = new ServerSocket(pport);
-                int c = 0;
                 while (true){
                     Socket socket = serverSocket.accept();
-                    count++;
+                    //count++;
                     new GetAllVotes(socket).start();
                 }
             } catch (IOException e) {
@@ -364,6 +380,7 @@ public class Participant {
         }
     }
 
+    // this class describes the thread which receives from the other participants the lists with the new votes from the previous round
     private class GetAllVotes extends Thread {
 
         Socket socket;
@@ -385,18 +402,17 @@ public class Participant {
                 String[] voteStmt = message.split(" ");
                 System.out.println(message);
 
-                //System.out.println(votes.toString());
                 for(int i = 1; i<voteStmt.length; i=i+2) {
-                    //System.out.println();
+                    // add all votes to the HashMap of votes in order to fill in any missing votes
                     votes.put(Integer.parseInt(voteStmt[i]), voteStmt[i+1]);
                 }
 
-                //System.out.println(votes.toString());
             } catch (IOException e) {
             }
         }
     }
 
+    // this class describes the thread which sends the list of all the votes received in the previous round to all of the other participants
     private class SendAllVotes extends Thread {
 
         SendAllVotes(){}
@@ -407,6 +423,7 @@ public class Participant {
 
             for (Integer p : participants) {
                 try {
+                    // create new socket to connect to other participants
                     Socket socket = new Socket("localhost", p);
                     PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
 
@@ -417,6 +434,8 @@ public class Participant {
                     }
 
                     out.println(message);
+
+                    System.out.println("PARTICIPANT " + pport + " sent the list of votes received in the previous round to PARTICIPANT " + p + ".");
 
                     sleep(500);
                     socket.close();
